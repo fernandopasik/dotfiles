cfeebe8073d98e48426b63ef5bb38be0ccd8b80e
Fernando Pasik <fernando@pasik.com.ar>
2017-04-22 19:08:32 +0100

    feat: Install OS X command line tools

--- install.sh
+++ install.sh
@@ -3,9 +3,14 @@
 function title {
   WIDTH=80
   PADDING=$(($WIDTH - ${#1}))
+  echo
   printf "\033[44;97m%*s\n" $WIDTH
   printf "%*s%*s\n" $(( $PADDING / 2 + ${#1} )) "$1" $(( $PADDING / 2 + $PADDING % 2 ))
   printf "%*s\n\033[0;0m" $WIDTH
+  echo
 }
 
 title "DOTFILES Installation"
+
+title "Install OS X Command Line Tools"
+xcode-select --install
