42bd429b3e3acf4afdffbe867075495df0f78f3e
Fernando Pasik <fernando@pasik.com.ar>
2017-04-24 22:12:23 +0100

    fix: Add end message to OS X setup

--- scripts/osx.sh
+++ scripts/osx.sh
@@ -89,3 +89,5 @@ defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool 
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
 # Add a context menu item for showing the Web Inspector in web views
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+echo OS X setup finished
