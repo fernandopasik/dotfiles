2479fc3c7a17ef0c4dade4519caa287974db40e0
Fernando Pasik <fernando@pasik.com.ar>
2017-05-02 17:22:56 +0100

    fix: Don't download automatically apps purchased

--- scripts/osx.sh
+++ scripts/osx.sh
@@ -92,8 +92,6 @@ defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 defaults write com.apple.SoftwareUpdate AutomaticDownload -int 1
 # Install System data files & security updates
 defaults write com.apple.SoftwareUpdate CriticalUpdateInstall -int 1
-# Automatically download apps purchased on other Macs
-defaults write com.apple.SoftwareUpdate ConfigDataInstall -int 1
 # Turn on app auto-update
 defaults write com.apple.commerce AutoUpdate -bool true
 # Allow the App Store to reboot machine on macOS updates
