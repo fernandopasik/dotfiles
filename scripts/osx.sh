190205e86485b48be27a4c8ce3b8d402ca696051
Fernando Pasik <fernando@pasik.com.ar>
2017-08-11 01:57:40 +0100

    fix: Remove deprecated amphetamine

--- scripts/osx.sh
+++ scripts/osx.sh
@@ -131,9 +131,4 @@ defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 defaults write com.divisiblebyzero.Spectacle StatusItemEnabled -bool false
 osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Spectacle.app", hidden:true, name:"Spectacle"}'
 
-defaults write com.if.Amphetamine Show\ Welcome\ Window -bool false
-defaults write com.if.Amphetamine Show\ Dock\ Icon -bool false
-defaults write com.if.Amphetamine Allow\ Display\ Sleep -bool true
-osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Amphetamine.app", hidden:true, name:"Amphetamine"}'
-
 echo OS X setup finished
