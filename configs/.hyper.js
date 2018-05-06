7a000ed4184efae1f12c6b201c8bcd0323fc04cd
Fernando Pasik <fernando@pasik.com.ar>
2018-05-07 00:39:12 +0100

    fix: use theme plugin for hyper

--- configs/.hyper.js
+++ configs/.hyper.js
@@ -1,32 +1,11 @@
 module.exports = {
   config: {
-    fontSize: 16,
-    fontFamily: '"Ubuntu Mono derivative Powerline"',
-    cursorColor: '#eee8d5',
-    foregroundColor: '#839496',
-    backgroundColor: '#002b36',
-    borderColor: '#073642',
+    fontSize: 14,
+    fontFamily: '"Roboto Mono for Powerline"',
     lineHeight: 1.2,
-    colors: [
-      '#073642',
-      '#dc322f',
-      '#859900',
-      '#b58900',
-      '#268bd2',
-      '#d33682',
-      '#2aa198',
-      '#eee8d5',
-      '#2D5C68',
-      '#cb4b16',
-      '#586e75',
-      '#657b83',
-      '#839496',
-      '#6c71c4',
-      '#93a1a1',
-      '#fdf6e3'
-    ]
   },
   plugins: [
+    'hyper-one-dark',
     'hyperlinks',
     'hyper-quit'
   ],
