c945b02a5e9f80123f6fb15fb4cc03dae313d7f2
Fernando Pasik <fernando@pasik.com.ar>
2019-02-10 20:08:25 +0000

    fix: switch to yarn publish and login

--- scripts/npm.sh
+++ scripts/npm.sh
@@ -13,10 +13,6 @@ yarn global add \
   imagemin-cli \
   react-native-cli \
 
-# Setting up username
-npm set username $NPM_USERNAME
-npm set email $AUTHOR_EMAIL
-
 # Setting up default yarn init options
 yarn config set init-author-url $AUTHOR_URL
 yarn config set init-author-email $AUTHOR_EMAIL
@@ -25,5 +21,5 @@ yarn config set init-license UNLICENSED
 yarn config set init-version 0.0.0
 yarn config set init-private true
 
-# Login to npmjs.org
-npm adduser
+# Login to yarn
+yarn login
