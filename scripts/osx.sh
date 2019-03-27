ffa25ecf1cf8ba78dcc0cfb1ed037cbd846055b8
Fernando Pasik <fernando@pasik.com.ar>
2019-03-27 21:13:46 +0000

    refactor: update os x options

--- scripts/osx.sh
+++ scripts/osx.sh
@@ -33,9 +33,6 @@ defaults write com.apple.menuextra.battery ShowTime -string "YES"
 # Use a dark menu bar / dock
 defaults write NSGlobalDomain AppleInterfaceStyle -string "Dark"
 
-# Check for software updates daily, not just once per week
-defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
-
 ###############################################################################
 # Trackpad, mouse, keyboard, Bluetooth accessories, and input                 #
 ###############################################################################
@@ -106,19 +103,27 @@ chflags nohidden ~/Library
 /usr/libexec/PlistBuddy "$HOME/Library/Preferences/com.apple.finder.plist" -c 'Add "StandardViewSettings:ListViewSettings:calculateAllSizes" bool true'
 
 ###############################################################################
-# Dock, Dashboard, and hot corners                                            #
+# Dock & hot corners                                                          #
 ###############################################################################
 
-# Set the icon size of Dock items
-defaults write com.apple.dock tilesize -int 52
-# Change minimize/maximize window effect
-defaults write com.apple.dock mineffect -string "scale"
+# Enable highlight hover effect for the grid view of a stack (Dock)
+#defaults write com.apple.dock mouse-over-hilte-stack -bool true
+
+# Set the icon size of Dock items to 60 pixels
+defaults write com.apple.dock tilesize -int 60
+
 # Enable spring loading for all Dock items
 defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
+
+# Change minimize/maximize window effect
+defaults write com.apple.dock mineffect -string "scale"
+
 # Show indicator lights for open applications in the Dock
 defaults write com.apple.dock show-process-indicators -bool true
+
 # Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
+
 # Top left screen corner → Put display to sleep
 defaults write com.apple.dock wvous-tl-corner -int 10
 defaults write com.apple.dock wvous-tl-modifier -int 0
@@ -145,11 +150,13 @@ function addAppToDock {
 
 # Delete all dock apps
 defaults delete com.apple.dock persistent-apps
+
 # Add apps to Dock
 for app in "${apps[@]}"
 do
   addAppToDock "$app"
 done
+
 # Refresh Dock
 killall Dock
 
@@ -159,14 +166,19 @@ killall Dock
 
 # Enable the automatic update check
 defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
+
 # Check for software updates daily, not just once per week
 defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
 # Download newly available updates in background
 defaults write com.apple.SoftwareUpdate AutomaticDownload -int 1
+
 # Install System data files & security updates
 defaults write com.apple.SoftwareUpdate CriticalUpdateInstall -int 1
+
 # Turn on app auto-update
 defaults write com.apple.commerce AutoUpdate -bool true
+
 # Allow the App Store to reboot machine on macOS updates
 defaults write com.apple.commerce AutoUpdateRestartRequired -bool true
 
@@ -186,6 +198,7 @@ defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
+
 # Add a context menu item for showing the Web Inspector in web views
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 
