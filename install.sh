406f4cc131d88ee14b45fae07f614841b821a52d
Fernando <fernando@pasik.com.ar>
2019-04-27 03:41:03 +0100

    refactor: shell setup

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ title "DOTFILES Installation"
 . .brew
 . .git-setup
 . .macos
+. .shell
 
 title "Node Setup"
 nvm install --lts
@@ -16,17 +17,8 @@ nvm install --lts
 title "Yarn Setup"
 source $PWD/scripts/yarn.sh
 
-title "Install Fonts"
-source $PWD/scripts/fonts.sh
-
-title "Shell Setup"
-source $PWD/scripts/shell.sh
-
 title "VS Code Setup"
 source $PWD/vscode/setup.sh
-
-# Create empty profile file
-touch ~/.profile
 
 # Restart computer
 osascript -e 'tell app "loginwindow" to «event aevtrrst»'
