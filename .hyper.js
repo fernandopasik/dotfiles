bb0507c95e88e0f20b6414135063fda106ef7b4c
Fernando <fernando@pasik.com.ar>
2019-04-27 03:43:29 +0100

    refactor: move shell config files

---
+++ .hyper.js
@@ -0,0 +1,14 @@
+module.exports = {
+  config: {
+    updateChannel: 'stable',
+    fontSize: 14,
+    fontFamily: '"Roboto Mono for Powerline"',
+    lineHeight: 1.2,
+  },
+  plugins: [
+    'hyper-one-dark',
+    'hyperlinks',
+    'hyper-quit'
+  ],
+  localPlugins: []
+};
