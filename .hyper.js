ebaec7a3259295f00a2851684f5ab6638ac7a52b
Fernando Pasik <fernando@pasik.com.ar>
2020-02-19 20:53:15 +0000

    feat: use jetbrains mono font for hyper

--- .hyper.js
+++ .hyper.js
@@ -2,7 +2,7 @@ module.exports = {
   config: {
     updateChannel: 'stable',
     fontSize: 14,
-    fontFamily: '"Roboto Mono for Powerline"',
+    fontFamily: 'JetBrains Mono',
     lineHeight: 1.2,
   },
   plugins: ['hyper-one-dark', 'hyperlinks', 'hyper-quit'],
