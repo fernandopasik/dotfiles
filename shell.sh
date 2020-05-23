d4b9cbd348ccef58184ba29675890283c64af94c
Fernando Pasik <fernando@pasik.com.ar>
2020-05-23 23:49:53 +0100

    refactor: deprecate old fonts

--- shell.sh
+++ shell.sh
@@ -5,22 +5,6 @@ CURRENT_DIR=$PWD
 
 title "Shell Setup"
 
-# ------------------
-log "Install Fonts"
-# ------------------
-
-FONTS_DIR=~/Library/Fonts
-POWERLINE_FONTS_DIR=$FONTS_DIR/powerline/
-
-if [ ! -d $POWERLINE_FONTS_DIR/.git ]
-then
-  git clone --depth 1 https://github.com/powerline/fonts.git $POWERLINE_FONTS_DIR
-else
-  cd $POWERLINE_FONTS_DIR
-  git pull
-  cd $CURRENT_DIR
-fi
-
 log "Create empty profile file"
 touch ~/.profile
 
