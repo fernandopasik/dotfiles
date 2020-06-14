7ef19192773a02c7dc5487f974d930a3503b2012
Fernando Pasik <fernando@pasik.com.ar>
2020-06-14 19:46:32 +0100

    refactor: deprecate tools

--- dev.sh
+++ dev.sh
@@ -22,9 +22,6 @@ yarn global add \
   npm-check-updates \
   pure-prompt \
   yo \
-  firebase-tools \
-  imagemin-cli \
-  react-native-cli \
 
 # Setting up default yarn init options
 yarn config set init-author-url "$AUTHOR_URL"
