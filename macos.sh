d66aa89f0bc72efec4c17942e1246cd6ee06bcd3
Fernando Pasik <fernando@pasik.com.ar>
2020-09-24 21:54:19 +0100

    refactor: extract app store settings

--- macos.sh
+++ macos.sh
@@ -172,6 +172,11 @@ killall Dock
 # Mac App Store                                                               #
 ###############################################################################
 
+# Enable the WebKit Developer Tools in the Mac App Store
+defaults write com.apple.appstore WebKitDeveloperExtras -bool true
+
+# Enable Debug Menu in the Mac App Store
+defaults write com.apple.appstore ShowDebugMenu -bool true
 
 ###############################################################################
 # Software Update                                                             #
