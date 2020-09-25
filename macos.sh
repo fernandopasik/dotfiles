fa55bcfdfe8690f4331700954c2f3fefea61af6b
Fernando Pasik <fernando@pasik.com.ar>
2020-09-25 22:41:28 +0100

    feat: add time machine options

--- macos.sh
+++ macos.sh
@@ -244,6 +244,16 @@ done
 killall Dock
 
 ###############################################################################
+# Time Machine                                                                #
+###############################################################################
+
+# Prevent Time Machine from prompting to use new hard drives as backup volume
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+# Disable local Time Machine backups
+# hash tmutil &> /dev/null && sudo tmutil disablelocal
+
+###############################################################################
 # Activity Monitor                                                            #
 ###############################################################################
 
