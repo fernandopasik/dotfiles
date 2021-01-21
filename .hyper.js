dd825b858156dfb7c96e82b6610beb8185c942ce
Fernando Pasik <fernando@pasik.com.ar>
2021-01-21 00:28:26 +0000

    refactor: customize hyper theme

--- .hyper.js
+++ .hyper.js
@@ -4,6 +4,8 @@ module.exports = {
     verminal: {
       fontSize: 16,
       fontFamily: 'JetBrains Mono',
+      foregroundColor: '#c9d1d9',
+      backgroundColor: '#0d1117',
     },
   },
   plugins: ['hyperlinks', 'hyper-quit', 'verminal'],
