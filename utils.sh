7318731aab50ac5ab495ad4891b692f39ce2ca12
Fernando Pasik <fernando@pasik.com.ar>
2021-08-29 21:13:34 +0100

    style: sort functions in file

--- utils.sh
+++ utils.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+current_email() {
+  /usr/libexec/PlistBuddy -c "print :Accounts:0:AccountID" ~/Library/Preferences/MobileMeAccounts.plist
+}
+
+current_first_name() {
+  id -F | grep -o "^\S*"
+}
+
+current_name() {
+  id -F
+}
+
+current_user() {
+  whoami
+}
+
 heading() {
   BLUE='\033[0;34m'
   NC='\033[0m' # No Color
@@ -15,20 +31,4 @@ log() {
 prompt() {
   read -r input
   echo "${input:-$1}"
-}
-
-current_name() {
-  id -F
-}
-
-current_first_name() {
-  id -F | grep -o "^\S*"
-}
-
-current_user() {
-  whoami
-}
-
-current_email() {
-  /usr/libexec/PlistBuddy -c "print :Accounts:0:AccountID" ~/Library/Preferences/MobileMeAccounts.plist
 }
