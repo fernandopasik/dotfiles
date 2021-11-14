66d26ecd018b555e137a1aea2670d17e54068811
Fernando Pasik <fernando@pasik.com.ar>
2021-11-14 05:11:32 +0000

    refactor: update oh my zsh install script

--- shell.sh
+++ shell.sh
@@ -13,7 +13,7 @@ OH_MY_ZSH_DIR="$HOME"/.oh-my-zsh/
 
 if [ ! -d "$OH_MY_ZSH_DIR".git ]; then
   log "Installing..."
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 else
   log "Updating..."
   cd "$OH_MY_ZSH_DIR" || exit
