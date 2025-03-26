b0b4de0379c120275a4b0ee0d886072abd0de498
Fernando Pasik <fernando@pasik.com.ar>
2025-03-26 15:32:53 +0000

    build: deprecate npm tools

--- eslint.config.js
+++
@@ -1,8 +0,0 @@
-import prettier from 'eslint-config-prettier';
-import ymlPlugin from 'eslint-plugin-yml';
-
-export default [
-  ...ymlPlugin.configs['flat/recommended'],
-  ...ymlPlugin.configs['flat/prettier'],
-  prettier,
-];
