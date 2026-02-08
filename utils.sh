3c6fd4fc60220325ed80c1d777297e2fda428a00
Fernando Pasik <fernando@pasik.com.ar>
2026-02-08 13:55:13 +0000

    refactor(git): move color to alias

--- utils.sh
+++ utils.sh
@@ -47,7 +47,7 @@ EOF
 git_status() {
   git rev-parse --git-dir > /dev/null || return
   ORIGIN=$(git cbo 2>/dev/null)
-  ST="$(git -c color.ui=always cbf)${ORIGIN:+ $(git behind-ahead)}"
+  ST="$(git cbf)${ORIGIN:+ $(git behind-ahead)}"
   if [ -z "$(git sp)" ]; then
     ST="$ST ✅"
   else
