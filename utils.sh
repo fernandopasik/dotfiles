1a144e2b37a2842d7f89392721189d2990e5afeb
Fernando Pasik <fernando@pasik.com.ar>
2026-02-10 04:15:23 +0000

    refactor: only find dirs with .git

--- utils.sh
+++ utils.sh
@@ -3,12 +3,12 @@ repos() {
   [ "$1" = "-s" ] && { one_line=1; shift; } || one_line=0
   REPOS=~/repos/
   cd "$REPOS" || exit
-  dirs=$(find . -mindepth 1 -maxdepth 1 -type d | sed 's|^\./||' | sort)
+  dirs=$(find . -mindepth 2 -maxdepth 2 -name '.git' | sed 's|^\./||;s|/\.git$||' | sort)
   max=$(printf '%s\n' "$dirs" | awk '{ if(length>m) m=length } END{print m}')
   printf '%s\n' "$dirs" | while IFS= read -r d; do
     printf "%-*s  " "$max" "$d"
     [ "$one_line" -eq 0 ] && printf "\n"
-    [ -e "$d/.git" ] && ( cd "$d" && "$@" )
+    (cd "$d" && "$@")
     [ $# -ne 0 ] && printf "\n"
    done
 }
