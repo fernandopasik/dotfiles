fef4d0fca6899eaac81bb2c8892560b68489baef
Fernando Pasik <fernando@pasik.com.ar>
2026-02-10 03:35:53 +0000

    refactor: posix method

--- utils.sh
+++ utils.sh
@@ -3,8 +3,7 @@ repos() {
   [ "$1" = "-s" ] && { one_line=1; shift; } || one_line=0
   REPOS=~/repos/
   cd "$REPOS" || exit
-
-  dirs=$(find . -mindepth 1 -maxdepth 1 -type d -exec basename {} \; | sort)
+  dirs=$(find . -mindepth 1 -maxdepth 1 -type d | sed 's|^\./||' | sort)
   max=$(printf '%s\n' "$dirs" | awk '{ if(length>m) m=length } END{print m}')
 
   printf '%s\n' "$dirs" | while IFS= read -r d; do
