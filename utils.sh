e816dc93f4f10b3c04294d52243adede5c933d73
Fernando Pasik <fernando@pasik.com.ar>
2026-02-10 04:26:12 +0000

    refactor: shorten script

--- utils.sh
+++ utils.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 repos() {
   [ "$1" = "-s" ] && { one_line=1; shift; } || one_line=0
-  REPOS=~/repos/
-  cd "$REPOS" || exit
+  cd ~/repos/ || exit
   dirs=$(find . -mindepth 2 -maxdepth 2 -name '.git' | sed 's|^\./||;s|/\.git$||' | sort)
   max=$(printf '%s\n' "$dirs" | awk '{ if(length>m) m=length } END{print m}')
   printf '%s\n' "$dirs" | while IFS= read -r d; do
