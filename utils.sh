4fd42a9ab20d4db0702b5cac026b3a8a0929169d
Fernando Pasik <fernando@pasik.com.ar>
2026-02-10 04:31:28 +0000

    refactor: new lines and separators

--- utils.sh
+++ utils.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 repos() {
-  [ "$1" = "-s" ] && { one_line=1; shift; } || one_line=0
+  [ "$1" = "-s" ] && { sep=''; shift; } || sep='\n'
   cd ~/repos/ || exit
   dirs=$(find . -mindepth 2 -maxdepth 2 -name '.git' | sed 's|^\./||;s|/\.git$||' | sort)
   max=$(printf '%s\n' "$dirs" | awk '{ if(length>m) m=length } END{print m}')
   printf '%s\n' "$dirs" | while IFS= read -r d; do
-    printf "%-*s  " "$max" "$d"
-    [ "$one_line" -eq 0 ] && printf "\n"
+    printf "\n%b%-*s  %b" "$sep" "$max" "$d" "$sep"
     (cd "$d" && "$@")
-    [ $# -ne 0 ] && printf "\n"
-   done
+  done
 }
