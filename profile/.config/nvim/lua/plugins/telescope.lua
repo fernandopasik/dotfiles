88e6b68b71988b68c6ffe119eeea8c637cd4c85f
Fernando Pasik <fernando@pasik.com.ar>
2026-02-21 09:36:39 +0000

    feat(nvim): telescope plugin

---
+++ profile/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,7 @@
+return {
+  'nvim-telescope/telescope.nvim', version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  },
+}
