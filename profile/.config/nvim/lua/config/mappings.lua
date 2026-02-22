3044c6dfe716fa452b30d976aaebc30abbf126ec
Fernando Pasik <fernando@pasik.com.ar>
2026-02-22 10:18:29 +0000

    refactor(nvim): move telescope mappings

--- profile/.config/nvim/lua/config/mappings.lua
+++ profile/.config/nvim/lua/config/mappings.lua
@@ -1,5 +1,3 @@
-local builtin = require('telescope.builtin')
-
 local function map(mode, lhs, rhs, opts)
   -- set default value if not specify
   if opts.noremap == nil then
@@ -16,9 +14,3 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 map("n", "-", "<cmd>Oil<CR>", { desc = "Open parant directory in Oil" })
-
-map('n', '<leader>ff', builtin.find_files,  { desc = 'Find files' })
-map('n', '<leader>fg', builtin.live_grep,   { desc = 'Live grep' })
-map('n', '<leader>fb', builtin.buffers,     { desc = 'Find buffers' })
-map('n', '<leader>fh', builtin.help_tags,   { desc = 'Help tags' })
-map('n', '<leader>fw', builtin.grep_string, { desc = 'Grep word under cursor' })
--- profile/.config/nvim/lua/plugins/telescope.lua
+++ profile/.config/nvim/lua/plugins/telescope.lua
@@ -4,4 +4,25 @@ return {
     'nvim-lua/plenary.nvim',
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   },
+  config = function()
+    local telescope = require('telescope')
+    local builtin = require('telescope.builtin')
+
+    telescope.setup({
+      pickers = {
+        find_files = {
+          find_command = { 'fd', '--type', 'f', '--hidden' },
+        }
+      },
+      extensions = { fzf = {} }
+    })
+
+    telescope.load_extension('fzf')
+
+    vim.keymap.set('n', '<leader>ff', builtin.find_files,  { desc = 'Find files' })
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep,   { desc = 'Live grep' })
+    vim.keymap.set('n', '<leader>fb', builtin.buffers,     { desc = 'Find buffers' })
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags,   { desc = 'Help tags' })
+    vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = 'Grep word under cursor' })
+  end,
 }
